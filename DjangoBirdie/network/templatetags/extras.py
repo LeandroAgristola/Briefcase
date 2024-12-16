f60d262065395f9b7adf89f5527c84f4951f9d75
LeandroAgristola <leandro.agristola@gmail.com>
2024-12-15 22:15:32 -0300

    ad Django Mail project

--- DjangoBirdie/network/templatetags/extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def profile_picture_or_default(profile_picture):
-    if profile_picture:
-        return profile_picture.url
-    return '/static/network/user/default_profile.jpg'
